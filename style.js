5dfc55db2715a27133d6a46208aa8462522d666c
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2021-02-05 11:19:04 +0100

    Update style.js

--- style.js
+++ style.js
@@ -52,6 +52,7 @@
 				{ name: 'Énfasis 19', element: 'span', attributes: { 'class': 'bck-enfasis-19'} },
 				{ name: 'Énfasis 20', element: 'span', attributes: { 'class': 'bck-enfasis-20'} },
 				{ name: 'Énfasis 21', element: 'span', attributes: { 'class': 'bck-enfasis-21'} },
+				{ name: 'Énfasis 22', element: 'span', attributes: { 'class': 'bck-enfasis-22'} },
 
 
 				{ name: 'Lista Ordenada 01', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-1' } },
